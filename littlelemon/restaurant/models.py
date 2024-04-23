77fde8f6ddcc64e3654e718a1c39eb6d71942619
Daniyal <daniyalarshad666@gmail.com>
2024-04-23 15:14:35 +0500

    added mysql settings and new moddels

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Booking(models.Model):
+  name = models.CharField(max_length=255)
+  no_of_guests = models.SmallIntegerField(default=1)
+  booking_date = models.DateField(db_index=True)
+
+class Menu(models.Model):
+  title = models.CharField(max_length=255, db_index=True)
+  price = models.DecimalField(max_digits=10, decimal_places=2)
+  inventory = models.SmallIntegerField(default=0)
