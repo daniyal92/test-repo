7dbe79a7ae62effe15afa1577c1504faefcb47fe
Daniyal <daniyalarshad666@gmail.com>
2024-04-23 17:21:28 +0500

    add unit test for menuitem

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -10,3 +10,6 @@ class MenuItem(models.Model):
   title = models.CharField(max_length=255, db_index=True)
   price = models.DecimalField(max_digits=10, decimal_places=2)
   inventory = models.SmallIntegerField(default=0)
+
+  def get_item(self):
+    return f'{self.title} : {str(self.price)}'
